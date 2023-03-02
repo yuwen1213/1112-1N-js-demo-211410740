ab05b5f76888a81417be52e90cd1b40d29a7fe38
yuwen1213 <wunhy2580@gmail.com>
2023-03-02 20:24:24 +0800

    W03-P3: implement next button and show the sequence in console.log

--- app_40.js
+++ app_40.js
@@ -2,9 +2,9 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'Cheng Yu Wen',
+    job: 'Dentist Assistant',
+    img: 'https://hlbovfzvhsftjuylmwlc.supabase.co/storage/v1/object/public/demo-40/md_1N_img/photo.2.jpg',
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   }, 
   {
@@ -41,11 +41,28 @@ const randomBtn = document.querySelector('.random-btn');
 
 let currentItem = 0;
 
-window.addEventListener('DOMContentLoaded', () => {
-  const item = reviews[currentItem];
+// function shoeReview() {
+
+// }
+
+const showReview = (person) => {
+  const item = reviews[person];
   console.log('item', item);
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  showReview(currentItem);
 });
+
+nextBtn.addEventListener('click', () => {
+  currentItem++;
+  if(currentItem >= reviews.length ) {
+    currentItem = 0;
+  }
+  // console.log('currentItem', reviews[currentItem])
+  showReview(currentItem);
+})
