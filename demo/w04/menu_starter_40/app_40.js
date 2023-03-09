02c0e49f3830f360c974f02369d2209f6eba9af7
yuwen1213 <wunhy2580@gmail.com>
2023-03-09 21:57:19 +0800

    W04-P3: show all filter buttons into html (section-center)

--- demo/w04/menu_starter_40/app_40.js
+++ demo/w04/menu_starter_40/app_40.js
@@ -38,7 +38,7 @@ const menu = [
     {
         id: 5,
         title: 'godzilla milkshake', 
-        category: 'shakes', 
+        category: 'desert', 
         price: 6.99,
         img: './images/item-2.jpeg',
         remote_img: '',
@@ -66,7 +66,7 @@ const displayMenuItems = (menu) => {
           <div class="item-info">
             <header>
               <h4>${item.title}</h4>
-              <h4 class="price">${item.price}}</h4>
+              <h4 class="price">${item.price}</h4>
             </header>
             <p class="item-text">
               ${item.desc}
@@ -75,14 +75,30 @@ const displayMenuItems = (menu) => {
         </article>
         `
     });
-    console.log('displayMenu before join', displayMenu);
+    // console.log('displayMenu before join', displayMenu);
     displayMenu = displayMenu.join('');
-    console.log('displayMenu after join', displayMenu);
+    // console.log('displayMenu after join', displayMenu);
     sectionCenter.innerHTML = displayMenu;
+}
+
+const categories = ['all', 'breakfast', 'lunch', 'dinner', 'shakes'];
+// const categories = ['all', ...new Set(menu.map((item) => item.category))];
+
+const displayMenuButtons = () => {
+    let menuButtons = categories.map((category) => {
+        return `
+        <button type="button" class="filter-btn" data-id=${category}>${category}</button>
+        `
+    });
+    console.log('displayButtons before join', menuButtons);
+    menuButtons = menuButtons.join('');
+    console.log('displayButtons after join', menuButtons);
+    btnContainer.innerHTML = menuButtons;
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     displayMenuItems(menu);
+    displayMenuButtons();
   });
 
 
