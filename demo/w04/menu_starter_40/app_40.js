92d5b74e2afd1cf24119b7e1db9432e1c73ff300
yuwen1213 <wunhy2580@gmail.com>
2023-03-09 19:59:21 +0800

    W04-P2: show all data in array menu into html (section-center)

--- demo/w04/menu_starter_40/app_40.js
+++ demo/w04/menu_starter_40/app_40.js
@@ -4,7 +4,7 @@ const menu = [
         title: 'blueberry pancake', 
         category: 'breakfast', 
         price: 15.59,
-        img: './images/my-1.jpeg',
+        img: './images/my-1.jpg',
         remote_img: '',
         desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat magni ipsam, at sequi maiores sit. Fuga non provident ipsa repellendus dolorem eveniet, ducimus iure cumque officiis corrupti deleniti ab libero ipsam, impedit nulla facere. Blanditiis dolore id minima voluptates maiores.',
     },
@@ -13,7 +13,7 @@ const menu = [
         title: 'salad', 
         category: 'lunch', 
         price: 13.99,
-        img: './images/my-2.jpeg',
+        img: './images/my-2.jpg',
         remote_img: '',
         desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat magni ipsam, at sequi maiores sit. Fuga non provident ipsa repellendus dolorem eveniet, ducimus iure cumque officiis corrupti deleniti ab libero ipsam, impedit nulla facere. Blanditiis dolore id minima voluptates maiores.',
     },
@@ -22,7 +22,7 @@ const menu = [
         title: 'penne pasta', 
         category: 'dinner', 
         price: 6.99,
-        img: './images/my-3.jpeg',
+        img: './images/my-3.jpg',
         remote_img: '',
         desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat magni ipsam, at sequi maiores sit. Fuga non provident ipsa repellendus dolorem eveniet, ducimus iure cumque officiis corrupti deleniti ab libero ipsam, impedit nulla facere. Blanditiis dolore id minima voluptates maiores.',
     },
@@ -57,3 +57,32 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 const btnContainer = document.querySelector('btn-container');
+
+const displayMenuItems = (menu) => {
+    let displayMenu = menu.map( (item) => {
+        return `
+        <article class="menu-item">
+          <img src=${item.img} alt=${item.tittle} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>
+        `
+    });
+    console.log('displayMenu before join', displayMenu);
+    displayMenu = displayMenu.join('');
+    console.log('displayMenu after join', displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayMenuItems(menu);
+  });
+
+
