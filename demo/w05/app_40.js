fe7632e0a0500d487aefe7e7ea389651a0c2d831
yuwen1213 <wunhy2580@gmail.com>
2023-03-16 19:55:26 +0800

    W05-P2: implement all filter buttons with all tests in console.log

--- demo/w05/app_40.js
+++ demo/w05/app_40.js
@@ -1,13 +1,16 @@
-import {menu} from './data.js'
+import { menu } from './data.js'
 
-const sectionCenter = document.querySelector('.section-center');
-const btnContainer = document.querySelector('btn-container');
+const categories = ['all', ...new Set(menu.map((item) => item.category))];
+console.log(categories);
+
+const sectionCenter = document.querySelector('.section-center');//doucment裡面=>DOMC...
+const btnContainer = document.querySelector('.btn-container');
 
 const displayMenuItems = (menu) => {
     let displayMenu = menu.map( (item) => {
         return `
         <article class="menu-item">
-          <img src=${item.img} alt=${item.tittle} class="photo" />
+          <img src=${item.img} alt=${item.title} class="photo" />
           <div class="item-info">
             <header>
               <h4>${item.title}</h4>
@@ -19,31 +22,47 @@ const displayMenuItems = (menu) => {
           </div>
         </article>
         `
-    });
-    // console.log('displayMenu before join', displayMenu);
+    })
+    
+    // 除錯用 console.log('displayMenu', displayMenu);
     displayMenu = displayMenu.join('');
     // console.log('displayMenu after join', displayMenu);
     sectionCenter.innerHTML = displayMenu;
+
 }
 
-// const categories = ['all', 'breakfast', 'lunch', 'dinner', 'desert', 'shakes'];
-const categories = ['all', ...new Set(menu.map((item) => item.category))];
-
-const displayMenuButtons = () => {
-    let menuButtons = categories.map((category) => {
+const displaycategoriesItems = (button) => {
+    let displayButton = button.map( (item) => {
         return `
-        <button type="button" class="filter-btn" data-id=${category}>${category}</button>
+        <button type="button" class="filter-btn" data-id="${item}">${item}</button>
         `
     });
-    console.log('displayButtons before join', menuButtons);
-    menuButtons = menuButtons.join('');
-    console.log('displayButtons after join', menuButtons);
-    btnContainer.innerHTML = menuButtons;
+    // console.log('displayButton', displayButton);
+    displayButton = displayButton.join('');
+    // console.log('displayButton after join', displayButton);
+    btnContainer.innerHTML = displayButton;
+    const filterBtns = document.querySelectorAll('.filter-btn');
+    console.log('filterBtns', filterBtns);
+    filterBtns.forEach( (btn) => {
+        btn.addEventListener('click', (e) => {
+            console.log('data-id', e.currentTarget.dataset.id);
+            const category = e.currentTarget.dataset.id;
+            const filterMenu = menu.filter( (item) => item.category === category);
+            if(category === 'all'){
+                console.log('all', menu)
+                displayMenuItems(menu);
+            }else{
+                console.log(category, filterMenu);
+                displayMenuItems(filterMenu)
+            }
+        });
+    });
 }
+
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
     displayMenuItems(menu);
-    displayMenuButtons();
-  });
-
-
+    displaycategoriesItems(categories);
+});
