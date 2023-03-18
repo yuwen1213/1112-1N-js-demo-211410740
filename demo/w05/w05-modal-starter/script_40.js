1b2c100a3158c55ebd9a754ad45ce14930c06fa7
yuwen1213 <wunhy2580@gmail.com>
2023-03-18 20:31:35 +0800

    W05-P4: call closeModal to close modal, overlay; press ESC key will call closeModal as well01~

--- demo/w05/w05-modal-starter/script_40.js
+++ demo/w05/w05-modal-starter/script_40.js
@@ -6,13 +6,35 @@ const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log('modal', modal);
-console.log('shoe-modal', btnsOpenModal);
 
-const openModal = () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+const closeModal = () => {
+    console.log('close');
+    modal.forEach((item) => {
+        item.classList.add('hidden');
+    });
+    overlay.classList.add('hidden');
 }
 
-for (let i=0; i<btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal)
+for(let i=0; i<btnsOpenModal.length; i++ ) {
+    btnsOpenModal[i].addEventListener('click', () => {
+        modal[i].classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
 }
+
+for(let i=0; i<btnCloseModal.length; i++ ) {
+    btnCloseModal[i].addEventListener('click', () => {
+        console.log('close');
+        modal[i].classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+}
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !overlay.classList.contains('hidden'))
+    {
+        closeModal();
+    }
+});
