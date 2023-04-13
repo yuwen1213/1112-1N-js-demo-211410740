4212c87e7270f97ca28e899673045afea8eb5315
yuwen1213 <wunhy2580@gmail.com>
2023-04-13 21:40:43 +0800

    mega menu for class demo

--- js/classdemo_40.js
+++ js/classdemo_40.js
@@ -1,11 +1,20 @@
 const showClassDemo = (week) => {
-    const showDemo = document.querySelector('.demo-section');
+    const showDemo = document.querySelector('.banner-section');
     switch(week){
         case 'w1':
-            showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100vh" />`
+            showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100%" />`
             break;
         case 'w2':
-            showDemo.innerHTML = `<iframe src='./demo/w02-tictaktoe/index.html' width="100%" height="100vh" />`
+            showDemo.innerHTML = `<iframe src='./demo/w02-tictactoe/tictactoe_40.html' width="100%" height="100%" />`
             break;
+        // case 'w3':
+        //     showDemo.innerHTML = `<iframe src='./demo/w03_review/index.html' width="100%" height="100%" />`
+        //     break;
+        // case 'w4':
+        //     showDemo.innerHTML = `<iframe src='./demo/w04-menu/index.html' width="100%" height="100%" />`
+        //     break;
+        // case 'w5':
+        //     showDemo.innerHTML = `<iframe src='./demo/w05-menu/index.html' width="100%" height="100%" />`
+        //     break;
     }
 }
