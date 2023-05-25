797516c57fedd7e66468bbca85e6244b4a19ee42
yuwen1213 <wunhy2580@gmail.com>
2023-05-25 19:05:08 +0800

    W15-P1: prepare menu items for API project in Vercel

--- demo/md/w14-product/products_40.md
+++ demo/md/w14-product/products_40.md
@@ -5,3 +5,8 @@
 ![w14-p3-1.png](https://hlbovfzvhsftjuylmwlc.supabase.co/storage/v1/object/public/demo-40/md_1N_img/w14-p3-1.png)
 
 ![w14-p3-2.png](https://hlbovfzvhsftjuylmwlc.supabase.co/storage/v1/object/public/demo-40/md_1N_img/w14-p3-2.png)
+
+```
+4ea6467 yuwen1213       Wed May 24 20:23:54 2023 +0800  W14-P3: Get products from url
+
+```
---
+++ demo/w15-stoe-product/js/app_40.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', async () => {
+  const url = 'https://www.thesportsdb.com/api/v1/json/3/eventsseason.php?id=4424&s=2019';
+try {
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log('data', data.events);
+  displayItems(data.events);
+} catch (error) {
+	console.error(error);
+}
+});
+
+const displayItems = (items) => {
+  const displayData = items
+    .map((item) => {
+      const {strEvent} = item;
+      return `<p>${strEvent}</p>`;
+    })
+    .join('');
+  const element = document.createElement('div');
+  element.innerHTML = displayData;
+  document.body.appendChild(element);
+};
